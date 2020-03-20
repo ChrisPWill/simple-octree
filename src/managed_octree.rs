89d06059454ff380951be470249355951ddcadce
Chris Williams <chris@chrispwill.com>
2020-03-20 15:34:11 +1100

    Add functions for getting octree length

--- src/managed_octree.rs
+++ src/managed_octree.rs
@@ -1,25 +1,26 @@
 use super::Octree;
+use len_trait::{Empty, Len};
 use std::borrow::{Borrow, BorrowMut};
 
 pub type ManagedOctree<D> = Octree<ManagedOctreeData<D>>;
 
 pub struct ManagedOctreeData<D>
 where
-    D: Default,
+    D: Default + Empty + Len,
 {
     data: D,
 }
 
 impl<D> Default for ManagedOctreeData<D>
 where
-    D: Default,
+    D: Default + Empty + Len,
 {
     fn default() -> Self { Self { data: D::default() } }
 }
 
 impl<D> ManagedOctreeData<D>
 where
-    D: Default,
+    D: Default + Empty + Len,
 {
     #[must_use]
     pub fn new() -> Self { Self::default() }
@@ -31,4 +32,32 @@ where
     /// Gets a mutable reference to the underlying data in the node.
     #[must_use]
     pub fn get_data_mut(&mut self) -> &mut D { self.data.borrow_mut() }
+}
+
+impl<D> Empty for ManagedOctree<D>
+where
+    D: Default + Empty + Len,
+{
+    fn is_empty(&self) -> bool {
+        self.data.data.is_empty()
+            && self
+                .children
+                .iter()
+                .filter_map(Option::as_ref)
+                .all(|c| c.is_empty())
+    }
+}
+
+impl<D> Len for ManagedOctree<D>
+where
+    D: Default + Empty + Len,
+{
+    fn len(&self) -> usize {
+        let children_sum: usize = self
+            .children
+            .iter()
+            .filter_map(|c| c.as_ref().map(Len::len))
+            .sum();
+        self.data.data.len() + children_sum
+    }
 }
